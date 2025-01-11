416b12e5ca0f783f1b67b8460116319a9ca8530d
Yusuf Motara <y.motara@ru.ac.za>
2025-01-11 17:38:04 +0200

    Two tests failing.  Surprisingly good—but I haven't verified graph STRUCTURE yet.  Not sure if there is an error in actual code or in recognizedWords; will have to investigate.

--- tests/DAWG_tests.elm
+++ tests/DAWG_tests.elm
@@ -91,4 +91,143 @@ suite =
           |> D.recognizedWords
           |> Expect.equal [randomlyGeneratedString]
       ]
+    , describe "is stress-tested with"
+      [ test "what-phat" <|
+        \_ ->
+          D.empty
+          |> D.addString "what"
+          |> D.addString "phat"
+          |> D.recognizedWords
+          |> Expect.equal ["phat", "what"]
+      , test "what's up-whotsup-wassup-whatsapp-wazzup" <|
+        \_ ->
+          D.empty
+          |> D.addString "what's up"
+          |> D.addString "whotsup"
+          |> D.addString "wassup"
+          |> D.addString "whatsapp"
+          |> D.addString "wazzup"
+          |> D.recognizedWords
+          |> Expect.equal ["wassup", "wazzup", "what's up", "whatsapp", "whotsup"]
+      , test "able-unable-disable" <|
+        \_ ->
+          D.empty
+          |> D.addString "able"
+          |> D.addString "unable"
+          |> D.addString "disable"
+          |> D.recognizedWords
+          |> Expect.equal ["able", "disable", "unable"]
+      , test "tap-tar-top" <|
+        \_ ->
+          D.empty
+          |> D.addString "tap"
+          |> D.addString "tar"
+          |> D.addString "top"
+          |> D.recognizedWords
+          |> Expect.equal ["tap", "tar", "top"]
+      , test "nation-action" <|
+        \_ ->
+          D.empty
+          |> D.addString "nation"
+          |> D.addString "action"
+          |> D.recognizedWords
+          |> Expect.equal ["action", "nation"]
+      , test "nation-action-function" <|
+        \_ ->
+          D.empty
+          |> D.addString "nation"
+          |> D.addString "action"
+          |> D.addString "function"
+          |> D.recognizedWords
+          |> Expect.equal ["action", "function", "nation"]
+      , test "nation-action-function-functionary" <|
+        \_ ->
+          D.empty
+          |> D.addString "nation"
+          |> D.addString "action"
+          |> D.addString "function"
+          |> D.addString "functionary"
+          |> D.recognizedWords
+          |> Expect.equal ["action", "function", "functionary", "nation"]
+      , test "nation-action-function-functionary-native" <|
+        \_ ->
+          D.empty
+          |> D.addString "nation"
+          |> D.addString "action"
+          |> D.addString "function"
+          |> D.addString "functionary"
+          |> D.addString "native"
+          |> D.recognizedWords
+          |> Expect.equal ["action", "function", "functionary", "nation", "native"]
+      , test "nation-function-functionary" <|
+        \_ ->
+          D.empty
+          |> D.addString "nation"
+          |> D.addString "function"
+          |> D.addString "functionary"
+          |> D.recognizedWords
+          |> Expect.equal ["function", "functionary", "nation"]
+      , test "fred-freddy" <|
+        \_ ->
+          D.empty
+          |> D.addString "fred"
+          |> D.addString "freddy"
+          |> D.recognizedWords
+          |> Expect.equal ["fred", "freddy"]
+      , test "fred-freddy-frederick" <|
+        \_ ->
+          D.empty
+          |> D.addString "fred"
+          |> D.addString "freddy"
+          |> D.addString "frederick" |> D.debugDAWG "graph"
+          |> D.recognizedWords
+          |> Expect.equal ["fred", "freddy", "frederick"]
+      , test "nation-action-nativity-activity" <|
+        \_ ->
+          D.empty
+          |> D.addString "nation"
+          |> D.addString "action"
+          |> D.addString "nativity"
+          |> D.addString "activity"
+          |> D.recognizedWords
+          |> Expect.equal ["action", "activity", "nation", "nativity"]
+      , test "nation-action-nativity-activity-act" <|
+        \_ ->
+          D.empty
+          |> D.addString "nation"
+          |> D.addString "action"
+          |> D.addString "nativity"
+          |> D.addString "activity"
+          |> D.addString "act"
+          |> D.recognizedWords
+          |> Expect.equal ["act", "action", "activity", "nativity", "nation"]
+      , test "x-y" <|
+        \_ ->
+          D.empty
+          |> D.addString "x"
+          |> D.addString "y"
+          |> D.recognizedWords
+          |> Expect.equal ["x", "y"]
+      , test "tark-tavk" <|
+        \_ ->
+          D.empty
+          |> D.addString "tark"
+          |> D.addString "tavk"
+          |> D.recognizedWords
+          |> Expect.equal ["tark", "tavk"]
+      , test "tark-turkey" <|
+        \_ ->
+          D.empty
+          |> D.addString "tark"
+          |> D.addString "turkey"
+          |> D.recognizedWords
+          |> Expect.equal ["tark", "turkey"]
+      , test "tark-shark" <|
+        \_ ->
+          D.empty
+          |> D.addString "tark"
+          |> D.addString "shark"
+          |> D.recognizedWords
+          |> Expect.equal ["shark", "tark"]
+      ]
     ]
