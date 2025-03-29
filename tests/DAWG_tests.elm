75ad2f1b85b74692db5b26df0cb3b8dfa6b6a978
Yusuf Motara <y.motara@ru.ac.za>
2025-03-29 13:34:49 +0100

    Two more tests, and more streamlined tests

--- tests/DAWG_tests.elm
+++ tests/DAWG_tests.elm
@@ -50,351 +50,134 @@ cheapTestForWords : List String -> Expect.Expectation
 cheapTestForWords words =
   czech [words] (expectedWords words)
 
+testWith : String -> Test
+testWith s =
+  test s <| \_ -> standardTestForWords (String.split "-" s)
+
+testWithInputs : List String -> List Test
+testWithInputs = List.map testWith
+
 suite : Test
 suite =
   describe "The DAWG module"
     [ -- Nest as many descriptions as you like.
-    describe "handles construction correctly when given"
-      [
-        test "px-pxa-pya-pya" <|
-        \_ ->
-          standardTestForWords ["px", "pxa", "pya", "pya"]
-      , test "pxa-py-qya" <|
-        \_ ->
-          standardTestForWords ["pxa", "py", "qya"]
-      , test "pxa-py-pya-pya" <|
-        \_ ->
-          standardTestForWords ["pxa", "py", "pya", "pya"]
-      , test "py-pya-pya" <|
-        \_ ->
-          standardTestForWords ["py", "pya", "pya"]
-      , test "py-pya" <|
-        \_ ->
-          standardTestForWords ["py", "pya"]
-      , test "px-pxa-pya-py" <|
-        \_ ->
-          standardTestForWords ["px", "pxa", "pya", "py"]
-      , test "pya-py-py" <|
-        \_ ->
-          standardTestForWords ["pya", "py", "py"]
-      , test "py-py" <|
-        \_ ->
-          standardTestForWords ["py", "py"]
-      , test "pxxa-pyy-pyya" <|
-        \_ ->
-          standardTestForWords ["pxxa", "pyy", "pyya"]
-      , test "pxxa-pyya-py" <|
-        \_ ->
-          standardTestForWords ["pxxa", "pyya", "py"]
-      , test "pxaab-pya-pyaab" <|
-        \_ ->
-          standardTestForWords ["pxaab", "pya", "pyaab"]
-      , test "pxba-py-pyyba" <|
-        \_ ->
-          standardTestForWords ["pxba", "py", "pyyba"]
-      , test "pxba-py-pyba" <|
-        \_ ->
-          standardTestForWords ["pxba", "py", "pyba"]
-      , test "pxa-py-pya-pr-pra" <|
-        \_ ->
-          standardTestForWords ["pxa", "py", "pya", "pr", "pra"]
-      , test "ps-s" <|
-        \_ ->
-          standardTestForWords ["ps", "s"]
-      , test "aps-as" <|
-        \_ ->
-          standardTestForWords ["aps", "as"]
-      , test "aps-ps" <|
-        \_ ->
-          standardTestForWords ["aps", "ps"]
-      , test "apqzs-as" <|
-        \_ ->
-          standardTestForWords ["apqzs", "as"]
-      , test "apqzs-azs" <|
-        \_ ->
-          standardTestForWords ["apqzs", "azs"]
-      , test "ate-create" <|
-        \_ ->
-          standardTestForWords ["ate", "create"]
-      , test "bars-ballad-bxrs" <|
-        \_ ->
-          standardTestForWords ["bars", "ballad", "bxrs"]
-      , test "bats-bars-bxrs" <|
-        \_ ->
-          standardTestForWords ["bats", "bars", "bxrs"]
-      , test "naton-acton-natit-actit" <|
-        \_ ->
-          standardTestForWords ["naton", "acton", "natit", "actit"]
-      , test "aton-cton-atit-ctit" <|
-        \_ ->
-          standardTestForWords ["aton", "cton", "atit", "ctit"]
-      , test "ato-cto-ati" <|
-        \_ ->
-          standardTestForWords ["ato", "cto", "ati"]
-      , test "ato-cto-at" <|
-        \_ ->
-          standardTestForWords ["ato", "cto", "at"]
-      , test "ato-cto-ati-cti" <|
-        \_ ->
-          standardTestForWords ["ato", "cto", "ati", "cti"]
-      , test "ato-cto" <|
-        \_ ->
-          standardTestForWords ["ato", "cto"]
-      , test "pqt-zvt-pqr" <|
-        \_ ->
-          standardTestForWords ["pqt", "zvt", "pqr"]
-      , test "pqt-zqr-pvr-pvt" <|
-        \_ ->
-          standardTestForWords ["pqt", "zqr", "pvr", "pvt"]
-      , test "pqt-prt-pqr-zvt" <|
-        \_ ->
-          standardTestForWords ["pqt", "prt", "pqr", "zvt"]
-      , test "pqt-zvt-zvr-pqr" <|
-        \_ ->
-          standardTestForWords ["pqt", "zvt", "zvr", "pqr"]
-      , test "pqt-zvt-pqarcz-pqr" <|
-        \_ ->
-          standardTestForWords ["pqt", "zvt", "pqarcz", "pqr"]
-      , test "pqt-zvt-zvarc-pqr" <|
-        \_ ->
-          standardTestForWords ["pqt", "zvt", "zvarc", "pqr"]
-      , test "pqt-zvt-pqrr" <|
-        \_ ->
-          standardTestForWords ["pqt", "zvt", "pqrr"]
-      , test "pqt-zvt-zvxt" <|
-        \_ ->
-          standardTestForWords ["pqt", "zvt", "zvxt"]
-      , test "x-y-xx" <|
-        \_ ->
-          standardTestForWords ["x", "y", "xx"]
-      , test "kxto-pzto-pzt" <|
-        \_ ->
-          standardTestForWords ["kxto", "pzto", "pzt"]
-      , test "crv-ax-cx-arv" <|
-        \_ ->
-          standardTestForWords ["crv", "ax", "cx", "arv"]
-      , test "pato-qcto-pat" <|
-        \_ ->
-          standardTestForWords ["pato", "qcto", "pat"]
-      , test "pato-qcto-pati" <|
-        \_ ->
-          standardTestForWords ["pato", "qcto", "pati"]
-      , test "pato-peto-qcto-pati" <|
-        \_ ->
-          standardTestForWords ["pato", "peto", "qcto", "pati"]
-      , test "ato-cto-atoz" <|
-        \_ ->
-          standardTestForWords ["ato", "cto", "atoz"]
-      , test "pato-qcto-patoz" <|
-        \_ ->
-          standardTestForWords ["pato", "qcto", "patoz"]
-      , test "atpvnxb-ctpvnxb-atprxb-ctprxb-atprxy" <|
-        \_ ->
-          standardTestForWords ["atpvnxb", "ctpvnxb", "atprxb", "ctprxb", "atprxy"]
-      , test "xa-y-ya" <|
-        \_ ->
-          standardTestForWords ["xa", "y", "ya"]
-      , test "xa-y-yaa" <|
-        \_ ->
-          standardTestForWords ["xa", "y", "yaa"]
-      , test "xa-y-yaaa" <|
-        \_ ->
-          standardTestForWords ["xa", "y", "yaaa"]
-      , test "xa-y-yaaaa" <|
-        \_ ->
-          standardTestForWords ["xa", "y", "yaaaa"]
-      , test "av-rv-kv-kva" <|
-        \_ ->
-          standardTestForWords ["av", "rv", "kv", "kva"]
-      , test "rxa-ry-pva-py-pxa" <|
-        \_ ->
-          standardTestForWords ["rxa", "ry", "pva", "py", "pxa"]
-      , test "xav-pbt-pzv" <|
-        \_ ->
-          standardTestForWords ["xav", "pbt", "pzv"]
-      , test "park-qv-qsk" <|
-        \_ ->
-          standardTestForWords ["park", "qv", "qsk"]
-      , test "park-qv-qvrk" <|
-        \_ ->
-          standardTestForWords ["park", "qv", "qvrk"]
-      , test "an-tn-x-tx" <|
-        \_ ->
-          standardTestForWords ["an", "tn", "x", "tx"]
-      , test "x-y-am-xm" <|
-        \_ ->
-          standardTestForWords ["x", "y", "am", "xm"]
-      , test "x-b-bc-ac-bx" <|
-        \_ ->
-          standardTestForWords ["x", "b", "bc", "ac", "bx"]
-      , test "px-pb-pbc-pac-pbz" <|
-        \_ ->
-          standardTestForWords ["px", "pb", "pbc", "pac", "pbz"]
-      , test "x-b-bc-ac-bzo" <|
-        \_ ->
-          standardTestForWords ["x", "b", "bc", "ac", "bzo"]
-      , test "kp-gx-ax-gp" <|
-        \_ ->
-          standardTestForWords ["kp","gx","ax","gp"]
-      , test "owx-bwx-ovx-bvx-owy" <|
-        \_ ->
-          standardTestForWords ["owx","bwx","ovx","bvx","owy"]
-      , test "owx-bwx-ovx-bvx-bv" <|
-        \_ ->
-          standardTestForWords ["owx","bwx","ovx","bvx","bv"]
-      , test "be-dv-cv-a-de" <|
-        \_ ->
-          standardTestForWords ["be","dv","cv","a","de"]
-      , test "ttal-ntl-ntal" <|
-        \_ ->
-          standardTestForWords ["ttal","ntl","ntal"]
-      , test "ttal-tyal-ntl-ntal" <|
-        \_ ->
-          standardTestForWords ["ttal","tyal","ntl","ntal"]
-      , test "tt-ttal-nt-ntl-ntal" <|
-        \_ ->
-          standardTestForWords ["tt","ttal","nt","ntl","ntal"]
-      , test "tstabl-nstabl-nstl" <|
-        \_ ->
-          standardTestForWords ["tstabl","nstabl","nstl"]
-      , test "tsbl-nsbl-nsl" <|
-        \_ ->
-          standardTestForWords ["tsbl","nsbl","nsl"]
-      , test "nst-nsl-tst-tsl-nstl" <|
-        \_ ->
-          standardTestForWords ["nst", "nsl", "tst", "tsl", "nstl"]
-      , test "tst-tstabl-nst-nstabl-nstl" <|
-        \_ ->
-          standardTestForWords ["tst","tstabl","nst","nstabl","nstl"]
-      , test "teste-ne-neste" <|
-        \_ ->
-          standardTestForWords ["teste", "ne", "neste"]
-      , test "tere-cere-te" <|
-        \_ ->
-          standardTestForWords ["tere","cere","te"]
-      , test "teve-ceve-cyve-tyve-te" <|
-        \_ ->
-          standardTestForWords ["teve", "ceve", "cyve", "tyve", "te"]
-      , test "ayxpayx-byxpayx-ayxpbyx-byxpbyx-ayx" <|
-        \_ ->
-          standardTestForWords ["ayxpayx", "byxpayx", "ayxpbyx", "byxpbyx", "ayx"]
-      , test "ayxpayx-byxpayx-ayxpbyx-byxpbyx-ayxpayx" <|
-        \_ ->
-          standardTestForWords ["ayxpayx", "byxpayx", "ayxpbyx", "byxpbyx", "ayxpayx"]
-      , test "ayayx-byayx-aybyx" <|
-        \_ ->
-          standardTestForWords ["ayayx", "byayx", "aybyx"]
-      , test "what-phat" <|
-        \_ ->
-          standardTestForWords ["what", "phat"]
-      , test "what'sup-whotsup-wassup-whatsapp-wazzup" <|
-        \_ ->
-          standardTestForWords ["what'sup", "whotsup", "wassup", "whatsapp", "wazzup"]
-      , test "able-unable-disable" <|
-        \_ ->
-          standardTestForWords ["able", "unable", "disable"]
-      , test "tap-tar-top" <|
-        \_ ->
-          standardTestForWords ["tap", "tar", "top"]
-      , test "nation-action" <|
-        \_ ->
-          standardTestForWords ["nation", "action"]
-      , test "nation-action-function" <|
-        \_ ->
-          standardTestForWords ["nation", "action", "function"]
-      , test "nation-action-function-functionary" <|
-        \_ ->
-          standardTestForWords ["nation", "action", "function", "functionary"]
-      , test "nation-action-function-functionary-native" <|
-        \_ ->
-          standardTestForWords ["nation", "action", "function", "functionary", "native"]
-      , test "nation-function-functionary" <|
-        \_ ->
-          standardTestForWords ["nation", "function", "functionary"]
-      , test "fred-freddy" <|
-        \_ ->
-          standardTestForWords ["fred", "freddy"]
-      , test "fred-freddy-frederick" <|
-        \_ ->
-          standardTestForWords ["fred", "freddy", "frederick"]
-      , test "nation-action-nativity-activity" <|
-        \_ ->
-          standardTestForWords ["nation", "action", "nativity", "activity"]
-      , test "nation-action-nativity-activity-act" <|
-        \_ ->
-          standardTestForWords ["nation", "action", "nativity", "activity", "act"]
-      , test "x-y" <|
-        \_ ->
-          standardTestForWords ["x", "y"]
-      , test "tark-tavk" <|
-        \_ ->
-          standardTestForWords ["tark", "tavk"]
-      , test "tark-turkey" <|
-        \_ ->
-          standardTestForWords ["tark", "turkey"]
-      , test "tark-shark" <|
-        \_ ->
-          standardTestForWords ["tark", "shark"]
-      , test "tar-tap" <|
-        \_ ->
-          standardTestForWords ["tar", "tap"]
-      , test "task-tark" <|
-        \_ ->
-          standardTestForWords ["task", "tark"]
-      , test "task-tark-tork" <|
-        \_ ->
-          standardTestForWords ["task", "tark", "tork"]
-      , test "task-hork-terk" <|
-        \_ ->
-          standardTestForWords ["task", "hork", "terk"]
-      , test "phong-pring" <|
-        \_ ->
-          standardTestForWords ["phong", "pring"]
-      , test "phong-pring-pheng" <|
-        \_ ->
-          standardTestForWords ["phong", "pring", "pheng"]
-      , test "prong-pring-pheng-prin" <|
-        \_ ->
-          standardTestForWords ["prong", "pring", "pheng", "prin"]
-      , test "prong-pring-prin-phong" <|
-        \_ ->
-          standardTestForWords ["prong", "pring", "prin", "phong"]
-      , test "tar-tap-box" <|
-        \_ ->
-          standardTestForWords ["tar", "tap", "box"]
-      , test "try-pry-cry" <|
-        \_ ->
-          standardTestForWords ["try", "pry", "cry"]
-      , test "aoeuth-aontuh-th-aoentuh-aoentuh-ouh" <|
-        \_ ->
-          standardTestForWords ["aoeuth", "aontuh", "th", "aoentuh", "aoentuh", "ouh"]
-      , test "axax-bx-cx-cxax" <|
-        \_ ->
-          standardTestForWords ["axax", "bx", "cx", "cxax"]
-      , test "ad-ars-xrs" <|
-        \_ ->
-          standardTestForWords ["ad", "ars", "xrs"]
-      , test "skop-rakop-rap" <|
-        \_ ->
-          standardTestForWords ["skop", "rakop", "rap"]
-      , test "skoz-rakop-rap-raz" <|
-        \_ ->
-          standardTestForWords ["skoz", "rakop", "rap", "raz"]
-      , test "xaz-yaz-ytp" <|
-        \_ ->
-          standardTestForWords ["xaz", "yaz", "ytp"]
-      , test "ad-da-dba" <|
-        \_ ->
-          standardTestForWords ["ad", "da", "dba"]
-      , test "txl-tl" <|
-        \_ ->
-          standardTestForWords ["txl", "tl"]
-      , test "sy-spw-ow" <|
-        \_ ->
-          standardTestForWords ["sy", "spw", "ow"]
-      , test "aka-aman-aasn-abasn" <|
-        \_ ->
-          standardTestForWords ["aka", "aman", "aasn", "abasn"]
+    describe "handles construction correctly when given" <|
+      testWithInputs [
+        "px-pxa-pya-pya"
+      , "pxa-py-qya"
+      , "pxa-py-pya-pya"
+      , "py-pya-pya"
+      , "py-pya"
+      , "px-pxa-pya-py"
+      , "pya-py-py"
+      , "py-py"
+      , "pxxa-pyy-pyya"
+      , "pxxa-pyya-py"
+      , "pxaab-pya-pyaab"
+      , "pxba-py-pyyba"
+      , "pxba-py-pyba"
+      , "pxa-py-pya-pr-pra"
+      , "ps-s"
+      , "aps-as"
+      , "aps-ps"
+      , "apqzs-as"
+      , "apqzs-azs"
+      , "ate-create"
+      , "bars-ballad-bxrs"
+      , "bats-bars-bxrs"
+      , "naton-acton-natit-actit"
+      , "aton-cton-atit-ctit"
+      , "ato-cto-ati"
+      , "ato-cto-at"
+      , "ato-cto-ati-cti"
+      , "ato-cto"
+      , "pqt-zvt-pqr"
+      , "pqt-zqr-pvr-pvt"
+      , "pqt-prt-pqr-zvt"
+      , "pqt-zvt-zvr-pqr"
+      , "pqt-zvt-pqarcz-pqr"
+      , "pqt-zvt-zvarc-pqr"
+      , "pqt-zvt-pqrr"
+      , "pqt-zvt-zvxt"
+      , "x-y-xx"
+      , "kxto-pzto-pzt"
+      , "crv-ax-cx-arv"
+      , "pato-qcto-pat"
+      , "pato-qcto-pati"
+      , "pato-peto-qcto-pati"
+      , "ato-cto-atoz"
+      , "pato-qcto-patoz"
+      , "atpvnxb-ctpvnxb-atprxb-ctprxb-atprxy"
+      , "xa-y-ya"
+      , "xa-y-yaa"
+      , "xa-y-yaaa"
+      , "xa-y-yaaaa"
+      , "av-rv-kv-kva"
+      , "rxa-ry-pva-py-pxa"
+      , "xav-pbt-pzv"
+      , "park-qv-qsk"
+      , "park-qv-qvrk"
+      , "an-tn-x-tx"
+      , "x-y-am-xm"
+      , "x-b-bc-ac-bx"
+      , "px-pb-pbc-pac-pbz"
+      , "x-b-bc-ac-bzo"
+      , "kp-gx-ax-gp"
+      , "owx-bwx-ovx-bvx-owy"
+      , "owx-bwx-ovx-bvx-bv"
+      , "be-dv-cv-a-de"
+      , "ttal-ntl-ntal"
+      , "ttal-tyal-ntl-ntal"
+      , "tt-ttal-nt-ntl-ntal"
+      , "tstabl-nstabl-nstl"
+      , "tsbl-nsbl-nsl"
+      , "nst-nsl-tst-tsl-nstl"
+      , "tst-tstabl-nst-nstabl-nstl"
+      , "teste-ne-neste"
+      , "tere-cere-te"
+      , "teve-ceve-cyve-tyve-te"
+      , "ayxpayx-byxpayx-ayxpbyx-byxpbyx-ayx"
+      , "ayxpayx-byxpayx-ayxpbyx-byxpbyx-ayxpayx"
+      , "ayayx-byayx-aybyx"
+      , "what-phat"
+      , "what'sup-whotsup-wassup-whatsapp-wazzup"
+      , "able-unable-disable"
+      , "tap-tar-top"
+      , "nation-action"
+      , "nation-action-function"
+      , "nation-action-function-functionary"
+      , "nation-action-function-functionary-native"
+      , "nation-function-functionary"
+      , "fred-freddy"
+      , "fred-freddy-frederick"
+      , "nation-action-nativity-activity"
+      , "nation-action-nativity-activity-act"
+      , "x-y"
+      , "tark-tavk"
+      , "tark-turkey"
+      , "tark-shark"
+      , "tar-tap"
+      , "task-tark"
+      , "task-tark-tork"
+      , "task-hork-terk"
+      , "phong-pring"
+      , "phong-pring-pheng"
+      , "prong-pring-pheng-prin"
+      , "prong-pring-prin-phong"
+      , "tar-tap-box"
+      , "try-pry-cry"
+      , "aoeuth-aontuh-th-aoentuh-aoentuh-ouh"
+      , "axax-bx-cx-cxax"
+      , "ad-ars-xrs"
+      , "skop-rakop-rap"
+      , "skoz-rakop-rap-raz"
+      , "xaz-yaz-ytp"
+      , "ad-da-dba"
+      , "txl-tl"
+      , "sy-spw-ow"
+      , "aka-aman-aasn-abasn"
+      , "hazsu-hosu-ssu-hap"
+      , "pa-az-pxz"
       ]
     , describe "stress-testing via fuzzing"
       [ fuzz (Fuzz.listOfLengthBetween 2 8 (Fuzz.asciiStringOfLengthBetween 1 5)) "always recognizes exactly the unique short words that it is given" <|
