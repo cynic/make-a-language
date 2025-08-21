e7fae819f7895a07181d64ef12cb3242e1b2ae05
Yusuf Motara <y.motara@ru.ac.za>
2025-08-21 23:46:19 +0800

    Round-tripping; need more tests, but that'll wait until tomorrow, I think.

--- tests/Encoding.elm
+++ tests/Encoding.elm
@@ -23,47 +23,37 @@ mkAutomatonGraph nodeIds edges root =
   , root = root
   }
 
+czech root nodes edges =
+  let
+    v = mkAutomatonGraph nodes edges root
+  in
+    case serializeAutomatonGraph v |> D.decodeValue deserializeAutomatonGraph of
+      Ok v_ -> Expect.equal v v_
+      Err e -> Expect.fail (Debug.toString e)
+
 
 suite : Test
 suite =
   describe "Encoding & Decoding serialized automata"
     [ describe "Decoding: serialized string to AutomatonGraph"
-      [ test "parses simple transitions correctly" <|
-        \_ ->
-          Expect.equal
-            [(0, "!av", 1), (0, "b!vk!z", 2), (2, "p", 0)]
-            (mkAG_input "0-!av-1 0-b!vk!z-2 2-p-0")
-      , test "parses single transition" <|
-        \_ ->
-          Expect.equal
-            [(1, "abc", 2)]
-            (mkAG_input "1-abc-2")
-      , test "handles empty string" <|
-        \_ ->
-          Expect.equal [] (mkAG_input "")
-      , test "handles whitespace" <|
-        \_ ->
-          Expect.equal [(1, " ", 2)] (mkAG_input "1- -2")
-      , test "handles a + character" <|
+      [ test "handles a + character" <|
         \_ ->
           Expect.equal [(1, "+", 2)] (mkAG_input "1-++-2")
       ]
-    , describe "Encoding: can round-trip AutomatonGraph"
-      [ test "encodes simple transitions correctly" <|
+    , describe "Round-tripping"
+      [ test "handles simple transitions correctly" <|
         \_ ->
-          let
-            v =
-              mkAutomatonGraph
-                [0, 1, 2]
-                [ (0, 1, "!av")
-                , (0, 2, "b!vk!z")
-                , (2, 0, "p")
-                ]
-                0
-          in
-            case serializeAutomatonGraph v |> D.decodeValue deserializeAutomatonGraph of
-              Ok v_ -> Expect.equal v v_
-              Err e -> Expect.fail (Debug.toString e)
+          czech 0
+            [0, 1, 2]
+            [ (0, 1, "!av"), (0, 2, "b!vk!z"), (2, 0, "p") ]
+      , test "handles single transition" <|
+        \_ ->
+          czech 1 [1, 2] [(1, 2, "abc")]
+      , test "handles whitespace transitions" <|
+        \_ ->
+          czech 0
+            [0, 1, 2, 3]
+            [ (0, 1, "! "), (1, 2, " "), (2, 3, "p ") ]
       ]
     -- , test "handles references" <|
     --   \_ ->
